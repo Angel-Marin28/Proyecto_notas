dc44fe58740bf309d9d195a7437639fc3d4ad354
Angel Garcia Marin <isc18350309@gmail.com>
2022-04-07 16:54:52 -0500

    Dividiendo la interfaz con Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -17,32 +17,44 @@ const handleInputChange =(event) => {
 }
 
 
-const handleResetClick = (event) => {
-  setInputsState({ ...setInputsState, title:"", date:"", note:"" });
+const handleClickLimpiar = (event) => {
+  setInputsState({title:"", date:"", note:"" });
 };
 
   return (
-    <div className="App">
-      <h1>Notas</h1>
+    <div className="App container">
+      <div className = "row" > 
+        <div className = "col" >
+          <h3>Lista</h3>
+        </div>
 
-      <label>
-        Titulo
-      <input id="title" name="title" type="text" onChange={handleInputChange} value={inputsState.title} /> 
-      </label>
-      <br/>
-      <label>
+        <div className="col">
+        <h3>Notas</h3>
+        <label className="mb-2">
+          Titulo
+          <input id="title" name="title" type="text" onChange={handleInputChange} value={inputsState.title} /> 
+          </label>
+         <br /> 
+         <label className="mb-2">
         Fecha
       <input id="date" name="date" type="text" onChange={handleInputChange} value={inputsState.date} /> 
       </label>
       <br/>
-        <label>
+      <label>
         Nota
       <input id="note" name="note" type="text" onChange={handleInputChange} value={inputsState.note} /> 
-
-        <button className="btn btn-primary me-2" onClick={handleResetClick}>Borrar</button>
       </label>
-
+      </div>
     </div>
+    <hr />
+
+    <div className = "row" > 
+        <button className="btn btn-primary me-2" onClick={handleClickLimpiar}>Limpiar</button>
+        </div>
+        </div>
+
+     
+
   );
 }
 
