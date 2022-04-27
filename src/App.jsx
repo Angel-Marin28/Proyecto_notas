9682e12ed409cef7a432b93232b64702e9e40c7e
Angel Garcia Marin <isc18350309@gmail.com>
2022-04-26 22:18:51 -0500

    Botón guardar a través del almacenamiento local del navegador

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,97 @@
 import { useState } from "react";
 
-
-const App =() => {
+const App = () => {
   //hool -> use .....
   //stage ->useState
 
-  const [inputsState, setInputsState]= useState({
-    title:"",
-    date:"",
-    note:"",
+  const [inputsState, setInputsState] = useState({
+    title: "",
+    date: "",
+    note: "",
   });
 
+  const handleInputChange = (event) => {
+    setInputsState({ ...inputsState, [event.target.name]: event.target.value });
+  };
 
-const handleInputChange =(event) => { 
- setInputsState({ ...inputsState, [event.target.name]: event.target.value});
-}
+  const handleClickLimpiar = (event) => {
+    setInputsState({ title: "", date: "", note: "" });
+  };
 
-
-const handleClickLimpiar = (event) => {
-  setInputsState({title:"", date:"", note:"" });
-};
+  const handleClickGuardar = () => {
+    localStorage.setItem("notas", JSON.stringify(inputsState));
+  };
 
   return (
     <div className="App container">
-      <div className = "row" > 
-        <div className = "col" >
+      <div className="row">
+        <div className="col">
           <h3>Lista</h3>
         </div>
-
         <div className="col">
-        <h3>Notas</h3>
-        <label className="mb-2">
-          Titulo
-          <input id="title" name="title" type="text" onChange={handleInputChange} value={inputsState.title} /> 
+          <h3>Notas</h3>
+          <label className="mb-2" style={{ width: "100%" }}>
+            Titulo
+            <input
+              id="title"
+              name="title"
+              type="text"
+              onChange={handleInputChange}
+              value={inputsState.title}
+              style={{ width: "100%" }}
+            />
           </label>
-         <br /> 
-         <label className="mb-2">
-        Fecha
-      <input id="date" name="date" type="text" onChange={handleInputChange} value={inputsState.date} /> 
-      </label>
-      <br/>
-      <label>
-        Nota
-      <input id="note" name="note" type="text" onChange={handleInputChange} value={inputsState.note} /> 
-      </label>
+          <br />
+          <label className="mb-2" style={{ width: "100%" }}>
+            Fecha
+            <input
+              id="date"
+              name="date"
+              type="text"
+              onChange={handleInputChange}
+              value={inputsState.date}
+              style={{ width: "100%" }}
+            />
+          </label>
+          <br />
+          <label style={{ width: "100%" }}>
+            Nota
+            <input
+              id="note"
+              name="note"
+              type="text"
+              onChange={handleInputChange}
+              value={inputsState.note}
+              style={{ width: "100%" }}
+            />
+          </label>
+          <hr />
+          <div className="row">
+            <span className="col">
+              <button
+                type="button"
+                className="btn btn-primary me-2"
+                onClick={handleClickLimpiar}
+                style={{ width: "100%" }}
+              >
+                Limpiar
+              </button>
+            </span>
+            <span className="col">
+              <button
+                type="button"
+                className="btn btn-primary me-2"
+                onClick={handleClickGuardar}
+                style={{ width: "100%" }}
+              >
+                Guardar
+              </button> {" "}
+            </span>
+          </div>
+        </div>
       </div>
     </div>
-    <hr />
-
-    <div className = "row" > 
-        <button className="btn btn-primary me-2" onClick={handleClickLimpiar}>Limpiar</button>
-        </div>
-        </div>
-
-     
-
   );
-}
+};
 
 export default App;
